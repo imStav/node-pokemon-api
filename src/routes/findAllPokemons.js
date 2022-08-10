f3f06d6d348b3da6957bb51df385a207ba1593a5
stav <saprocontact@gmail.com>
2022-08-10 15:33:12 +0200

    Set advanced request filters & limits

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,8 +1,34 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    //Request filter
+    if(req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+
+      if(name.length < 2) {
+        const message = "La recherche doit au contenir moins 2 caractères."
+        return res.status(400).json({ message })
+      }
+
+      return Pokemon.findAndCountAll({ 
+        where: { //Sequelize operator WHERE
+          name: { // property of the Model Pokemon
+            [Op.like]: `%${name}%` // using LIKE operator to filter (similar to SQL LIKE)
+          }
+        },
+        order: ['name'], // Sort names alphabetically
+        limit: limit // Limits the request results to the user choice, or 5 max
+       })
+      .then(({count, rows}) => {
+        const message = `Il y a ${count} pokémon(s) qui correspondent à la recherce ${name}.`
+        res.json({ message, data: rows })
+      })
+    }
+    else {
+      Pokemon.findAll({ order: ['name'] })
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -12,5 +38,6 @@ module.exports = (app) => {
         const message = "La liste des pokémons n'a pas pu être récupérée. Réessayez dans quelques instants."
         res.status(500).json({message, data: error})
       })
+    }
   })
 }
